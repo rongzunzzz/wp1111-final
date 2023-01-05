1866b8aa4db38696da199773ffa530b6794f6882
rongzunzzz <huangrongzun@huangrongzundeMacBook-Pro.local>
2023-01-05 15:53:32 +0800

    forget to mod files b4 deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,35 @@ import bodyParser from 'body-parser';
 import db from "./db"
 import routes from "./routes/index"
 import cors from "cors"
+import path from 'path';
 
 
 const app = express();
-app.use(cors())
-db.connect();
+
 const port = process.env.PORT || 4000;
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors());    
+}
+
+// init middleware (跨 port 存取資源)
 app.use(bodyParser.json());
+
+// define routes
 app.use("/", routes);
+
+if (process.env.NODE_ENV === "production") {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, "../frontend", "build")));
+    app.get("/*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+    });
+}
+
+// connect DB
+db.connect();
 app.listen(port, () =>
-     console.log(`Example app listening on port ${port}!`),
+console.log(`Server loaded on port ${port}`)
 );
 
 
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,17 @@
 import axios from 'axios';
 
+// const instance = axios.create({
+//   baseURL: `http://localhost:4000/`,
+// });
+
+
+const API_ROOT = process.env.NODE_ENV === "production" ? 
+    "/" : `http://localhost:4000/`
+
 const instance = axios.create({
-  baseURL: `http://localhost:4000/`, 
-});
+    baseURL: API_ROOT
+})
 
 export default instance;
+
+// instance.get('/hi').then((data) => console.log(data));
